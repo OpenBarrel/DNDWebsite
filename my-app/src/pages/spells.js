107197e68a18b7a52aba5dedd56fac570cf51f88
Noah <noahberg29@gmail.com>
2024-06-15 17:24:37 -0500

    added toggleable class button

--- my-app/src/pages/spells.js
+++ my-app/src/pages/spells.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Spells(props) {
     const [spells, setSpells] = useState([]);
+    const [useClass, setUseClass] = useState([]);
     const spellLevel = props.level;
     const tabs = [0,1,2,3,4,5,6,7,8,9];
     let classFiltering = props.checkClass;
@@ -18,16 +19,18 @@ export default function Spells(props) {
       });
     
     }, []);
+
     
     // maps each spell for display, filters by spell level and optionally filters by class
     return (
       <div>
         <ul className='nav nav-tabs'>
             {tabs.map( (level) => (<li><a href={'#spells/' + level} >Level {level}</a></li>) )}
+            <button onClick={() => (setUseClass(!useClass))}>Use Classes: {useClass.toString()}</button>
         </ul>
         <ul>
           {spells.filter((spell) => ((spell.level === spellLevel)))
-          .filter((spell) => (classFiltering === true ? (spell.classes
+          .filter((spell) => (useClass === true ? (spell.classes
           .some(name => name.index === 'cleric')): true)).map((spell) => ( 
           <li key={spell.index}> {spell.name} - {spell.level === 0 ? 'Cantrip' : spell.level} - {spell.school.name}</li> 
         ))} 
